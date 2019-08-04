13fbf77867b21d80c205bdfe3492c3f112a26e17
huangyongchao <huangyongchao@emar.com>
2019-08-04 17:01:14 +0800

    基础配置

--- src/main/java/com/hrbc/business/BusinessApplication.java
+++ src/main/java/com/hrbc/business/BusinessApplication.java
@@ -3,6 +3,7 @@ package com.hrbc.business;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -14,8 +15,14 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @MapperScan({"com.hrbc.business.mapper"})
 public class BusinessApplication  extends SpringBootServletInitializer {
 
+
     public static void main(String[] args) {
         SpringApplication.run(BusinessApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BusinessApplication.class);
+    }
 }
+
