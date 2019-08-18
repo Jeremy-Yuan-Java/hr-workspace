098d1c54c43f823dfe4a7777d5bc7cbf5b8eac90
huangyongchao <huangyongchao@emar.com>
2019-08-18 11:51:08 +0800

    基础配置

---
+++ src/main/java/com/hrbc/business/conf/GlobalException.java
@@ -0,0 +1,22 @@
+package com.hrbc.business.conf;
+
+
+import com.hrbc.business.domain.common.ResponseDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class GlobalException {
+    private static final Logger logger = LoggerFactory.getLogger(GlobalException.class);
+
+    @ResponseBody
+    @ExceptionHandler
+    public ResponseDTO processException(Exception e) {
+        logger.error("GlobalException:", e);
+        return new ResponseDTO(false, "操作失败", null);
+
+    }
+}
