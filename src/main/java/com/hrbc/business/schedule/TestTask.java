0ed26ea6b5e20d8a1cf73ee144ab4bac588c4ced
dpb <dengpbs@163.com>
2019-08-31 01:12:43 +0800

    dpb 更新

--- src/main/java/com/hrbc/business/schedule/TestTask.java
+++ src/main/java/com/hrbc/business/schedule/TestTask.java
@@ -16,14 +16,14 @@ public class TestTask {
     private CandidatesResumeService resumeService;
 
     //@Scheduled(cron = "0 50 23 * * ?")
-    @PostConstruct
+    //@PostConstruct
     public void test() {
         // 1.将通过简历解析获取的数据 重新解析一遍，信息保存在 resumedetail 字段中
         // 注意此操作不需要 插入新的候选人信息
          // 2.将手动录入的信息重新 同步到简历详情相关表结构中
         //System.out.println("开始同步数据");
        // long start = new Date().getTime();
-        resumeService.updateSynchronizationCandidatesInfo();
+        // resumeService.updateSynchronizationCandidatesInfo();
        // long end = new Date().getTime();
        // System.out.println("同步结束:" + (end - start));
     }
