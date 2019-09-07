51a6be244a552305001486daef7a7434f84972e3
dpb <dengpbs@163.com>
2019-09-07 23:08:47 +0800

    dpb 更新

--- src/main/java/com/hrbc/business/schedule/TestTask.java
+++ src/main/java/com/hrbc/business/schedule/TestTask.java
@@ -16,13 +16,13 @@ public class TestTask {
     private CandidatesResumeService resumeService;
 
     //@Scheduled(cron = "0 50 23 * * ?")
-    @PostConstruct
+    //@PostConstruct
     public void test() {
         // 同步数据 根据现有的数据生成  CandidatesResume对象
-        System.out.println("开始同步数据");
+        /*System.out.println("开始同步数据");
         long start = new Date().getTime();
         resumeService.updateCandidatesInfo();
         long end = new Date().getTime();
-        System.out.println("同步结束:" + (end - start));
+        System.out.println("同步结束:" + (end - start));*/
     }
 }
